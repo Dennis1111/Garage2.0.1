ac73a30ddf85efeb884d9b19c0396671bccb886f
Dennis1111 <dennis.nilsson1111@gmail.com>
2018-06-04 01:18:37 +0200

    index with searchformmodel

---
+++ Garage2.0.1/Models/SearchFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Garage2._0._1.Models
+{
+    public enum Sorting{Descending,Ascending,None}
+
+    public class SearchFormModel
+    {
+        [DisplayName("Search Name")]
+        public String SearchName { get; set; }
+        public String Column { get; set; }
+        public Sorting SortingMethod { get; set; }
+    }
+}
